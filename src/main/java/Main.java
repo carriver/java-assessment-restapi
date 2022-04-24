0cd7768845cc4f75610763939d2e9c7f35d8dd55
Carlos Rivera <95212348+carriver@users.noreply.github.com>
2022-04-24 02:24:31 -0400

    Completed implementation of method getCountryDataFromAPI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,29 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Scanner;
-import org.json.JSONObject;
 
 public class Main {
 
-    public static String getCountryDataFromAPI (String country, String phoneNumber) throws Exception {
+    public static String getCountryDataFromAPI(String country, String phoneNumber) throws Exception {
+
+        // If country contains spaces, they are replaced by %20
+        if (country.contains(" ")) {
+            String[] pieces = country.split(" ");
+            country = "";
+
+            // Adds all the pieces except the last one
+            for (int i = 0; i < pieces.length - 1; i++) {
+                country += pieces[i] + "%20";
+            }
+            // Add last piece without the trailing %20
+            country += pieces[pieces.length - 1];
+        }
+
         String url = "https://jsonmock.hackerrank.com/api/countries?name="
                 + country;
 
@@ -21,9 +37,32 @@ public class Main {
             return "-1";
         }
 
+        JSONObject json = new JSONObject(jsonString);
+        JSONArray data = new JSONArray(json.opt("data").toString());
+
+        if (data.isEmpty()) {
+            return "-1";
+        }
+
+        json = new JSONObject(data.get(0).toString());
+
+        String capital = json.opt("capital").toString();
+
+        JSONArray callingCodesArray = new JSONArray(json.opt("callingCodes").toString());
+        String callingCode = callingCodesArray.get(callingCodesArray.length() - 1).toString();
+
+        if (capital.equals("null") || callingCodesArray.isEmpty()) {
+            return "-1";
+        }
+
+        String response = "The capital is " + capital
+                + " and the phone number is: +"
+                + callingCode + " " + phoneNumber + ".";
+
         in.close();
-        return jsonString;
+        return response;
     }
+
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
